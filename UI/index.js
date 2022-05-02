129bb8db8898e4bf3f36a53a7afdbd79b654d777
shuklashubhansh <shukla.shubhansh@gmail.com>
2022-05-02 18:19:48 +0530

    popup style changed from grid to flex. Popup animation added. Popup auto close functionality is conditioned.

--- UI/index.js
+++ UI/index.js
@@ -19,30 +19,30 @@ postdata = async () => {
     let name = document.querySelector('#givenname').value;
     let occupation = document.querySelector('#givenoccupation').value;
     if(!name || !occupation){
-        showPop("Please fill up all the details!");
+        showPop("Please fill up all the details!", true);
     } else {
         const obj = [{ name: name, occupation: occupation }];
         let response = await fetch("http://localhost:8080/writedata",
-        { method: 'POST', body: JSON.stringify(obj)}).catch((error)=>{showPop("Yikes! Unable to update the details.")});
+        { method: 'POST', body: JSON.stringify(obj)}).catch((error)=>{showPop("Yikes! Unable to update the details.", true)});
 
         if (response.status == 200) {
             let data = await response.json();
             window.location.reload();
         } else {
-            showPop(`Unable to update the details - ${response.status}`);
+            showPop(`Unable to update the details - ${response.status}`, true);
         }
     }
 }
 
 deletedata = async (data) => {
     let response = await fetch("http://localhost:8080/deletedata",
-    {method:'post', body: JSON.stringify(data)}).catch((error)=>{showPop("Yikes! Unable to delete.")});
+    {method:'post', body: JSON.stringify(data)}).catch((error)=>{showPop("Yikes! Unable to delete.", true)});
 
     if (response.status == 200) {
         let data = await response.json();
         window.location.reload();
     } else {
-        showPop(`Unable to delete - ${response.status}`);
+        showPop(`Unable to delete - ${response.status}`, true);
     }
 }
 
@@ -60,7 +60,7 @@ generateUI = (data, parent) => {
         d.appendChild(btn);
         parent.appendChild(d);
         btn.addEventListener('click', ()=>{event.stopPropagation(); data.splice(index,1); data = data.reverse(); deletedata(data)});
-        d.addEventListener('click',()=>showPop(`${item.name} is ${item.occupation} by profession.`));
+        d.addEventListener('click',()=>showPop(`${item.name} is ${item.occupation} by profession.`, false));
     })
 }
 
@@ -79,14 +79,17 @@ scrollUI = () => {
     window.scroll({top:0, behavior:"smooth"});
 }
 
-showPop = (message) => {
+showPop = (message, autoclose) => {
     clearTimeout(this.timeout);
-    popup.innerText=message;
-    popup.style.display='grid';
-    this.timeout = setTimeout(()=>{popup.style.display='none'},3000);
+    if(autoclose){
+        this.timeout = setTimeout(()=>{popup.style.display='none'},3000);
+     }
+    popup.childNodes[0].innerText=message;
+    popup.childNodes[1].addEventListener('click',()=>popup.style.display='none');
+    popup.style.display='flex';
 }
 
 handleError = (errorMessage, parentElement) => {
-    showPop(errorMessage);
+    showPop(errorMessage, true);
     parentElement.innerHTML="<H2 style='color:red;'>Something went wrong. Please try after sometime.</H2>";
 }
